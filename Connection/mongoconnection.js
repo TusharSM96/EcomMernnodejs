56d6cdcfeef03c88767e2479b86fd24745d89277
TusharSM96 <tusharshivajimulik1811@gmail.com>
2024-07-08 23:50:32 +0530

    mongo connection chages

--- Connection/mongoconnection.js
+++ Connection/mongoconnection.js
@@ -1,12 +1,6 @@
-// const mongoose = require('mongoose');
-// mongoose.connect('mongodb+srv://Tushar9963:Tushar@9963@ecomnode.pc4qbsw.mongodb.net/mydatabase?retryWrites=true&w=majority&appName=EcomNode')
-// mongoose.connect('mongodb+srv://tusharshivaji1811:Tushar%4078657865@ecomnode.pc4qbsw.mongodb.net/mydatabase?retryWrites=true&w=majority&appName=EcomNode')
-// mongoose.connect(process.env.MONGODBCONNECTION)
 const mongoose = require('mongoose');
-
 // Corrected MongoDB connection URI
-const mongoURI = 'mongodb+srv://Tushar9963:Tushar%409963@ecomnode.pc4qbsw.mongodb.net/mydatabase?retryWrites=true&w=majority&appName=EcomNode';
-
+const mongoURI = process.env.MONGODBCONNECTION;
 mongoose.connect(mongoURI).then(() => {
   console.log('MongoDB connected');
 }).catch(err => {
