3d4b4a2624feb3d62dd8e2f9384cb09b529354ab
TusharSM96 <tusharshivajimulik1811@gmail.com>
2024-07-08 23:42:50 +0530

    mongo connection chages

--- Connection/mongoconnection.js
+++ Connection/mongoconnection.js
@@ -1,3 +1,14 @@
-const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://tusharshivaji1811:Tushar%4078657865@ecomnode.pc4qbsw.mongodb.net/mydatabase?retryWrites=true&w=majority&appName=EcomNode')
+// const mongoose = require('mongoose');
+// mongoose.connect('mongodb+srv://Tushar9963:Tushar@9963@ecomnode.pc4qbsw.mongodb.net/mydatabase?retryWrites=true&w=majority&appName=EcomNode')
+// mongoose.connect('mongodb+srv://tusharshivaji1811:Tushar%4078657865@ecomnode.pc4qbsw.mongodb.net/mydatabase?retryWrites=true&w=majority&appName=EcomNode')
 // mongoose.connect(process.env.MONGODBCONNECTION)
+const mongoose = require('mongoose');
+
+// Corrected MongoDB connection URI
+const mongoURI = 'mongodb+srv://Tushar9963:Tushar%409963@ecomnode.pc4qbsw.mongodb.net/mydatabase?retryWrites=true&w=majority&appName=EcomNode';
+
+mongoose.connect(mongoURI).then(() => {
+  console.log('MongoDB connected');
+}).catch(err => {
+  console.error('MongoDB connection error:', err);
+});
