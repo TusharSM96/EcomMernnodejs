6945658be879d2aae61f1db4672e6377f276796c
TusharSM96 <tusharshivajimulik1811@gmail.com>
2024-07-08 22:45:48 +0530

    add vercel.json fiel config

--- Connection/mongoconnection.js
+++ Connection/mongoconnection.js
@@ -1,2 +1,3 @@
 const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGODBCONNECTION)
+mongoose.connect('mongodb+srv://tusharshivaji1811:Tushar%4078657865@ecomnode.pc4qbsw.mongodb.net/mydatabase?retryWrites=true&w=majority&appName=EcomNode')
+// mongoose.connect(process.env.MONGODBCONNECTION)
